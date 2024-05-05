75fb3232934bffe2c262255ea96586fa0c042b7f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-06 00:24:48 +0200

    php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>PHP Basics</title>
+	<h1>PHP Basics</h1>
+</head>
+<body>
+
+<?php
+
+$user = 123;
+$user = "Belle Bun Bunny";
+
+$data = [
+	"first" => 2, 
+	"second" => 3.99,
+	"third" => 0
+];
+
+$price = $data["first"];
+
+if ($price > 1) {
+
+	echo "Price is > 1";
+foreach ($data as $i => $val;
+	echo $i . " = " . $val;}
+?>
+</body>
+</html>
