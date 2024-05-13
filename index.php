276764b9c040f4477d152674c791a22e92d0aa33
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-13 13:14:19 +0000

    php

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 <head>
-    <title>hello world</title>
+    <title>PHP Basics</title>
+    <h1>PHP Basics</h1>
 </head>
+
 <body>
-  <?php
+    <?php
 
-    $name = "belle bun bunny";
+$user = 123;
+$user = "Belle Bun Bunny";
 
-    echo "hello " . $name;
+$data = ["first" => 2, "second" => 3.99," third" => 0
+];
 
+$price = $data["first"];
+
+if ($price = $data);{
+	echo "$price is > 1 $user";
+foreach ($data as $i => $val);
+	echo $i . " = " . $val;}
 ?>
 </body>
+
 </html>
