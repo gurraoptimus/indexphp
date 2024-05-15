827a7de6b64e3cc824dc42783cbaad3cb6b3c9fe
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-15 11:33:05 +0000

    php

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<html>
 
 <head>
-    <title>PHP Basics</title>
-    <h1>PHP Basics</h1>
+    <title></title>
 </head>
 
 <body>
-    <?php
-
-$user = 123;
-$user = "Belle Bun Bunny";
-
-$data = ["first" => 2, "second" => 3.99," third" => 0
-];
-
-$price = $data["first"];
-
-if ($price = $data);{
-	echo "$price is > 1 $user";
-foreach ($data as $i => $val);
-	echo $i . " = " . $val;}
-?>
+    <form action="./uploads" method="POST" enctype="multipart/form-data">
+        <input type="file" name="">
+        <button type="submit" name="submit">UPLOAD IMAGE</button>
+    </form>
 </body>
 
 </html>
