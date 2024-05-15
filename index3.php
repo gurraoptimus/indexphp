d054c6717e5a9369223b22b94b16961d883af973
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-15 11:31:34 +0000

    php

--- index3.php
+++ index3.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-    <form action="./upload.php" method="POST" enctype="multipart/form-data">
+    <form action="./uploads" method="POST" enctype="multipart/form-data">
         <input type="file" name="">
         <button type="submit" name="submit">UPLOAD IMAGE</button>
     </form>
