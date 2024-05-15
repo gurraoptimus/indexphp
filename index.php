2c8edc1910bd87bc592373e7702abc19ce197a98
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-15 11:50:37 +0000

    php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
     <form action="upload.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="">
+        <input type="file" name="file">
         <button type="submit" name="submit">UPLOAD IMAGE</button>
     </form>
 </body>
--- upload.php
+++ upload.php
@@ -1,7 +1,12 @@
 <?php
 if (isset($_POST["submit"])) {
 	$file = $_FILES["file"];
-print_r($file);
+	print_r($file);
+	
+	$fileName = $_FILES["file"]["name"];
+	$fileName = $_FILES["file"]["name"];
+	$fileName = $_FILES["file"]["name"];
+	$fileName = $_FILES["file"]["name"];
 	$fileName = $_FILES["file"]["name"];
 }
 ?>
